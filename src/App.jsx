ed00dacac6ab7f5ecadc7507552746ad81fd03bb
rocketmike12 <mg.korop@gmail.com>
2025-11-29 13:01:34 +0200

    (scss) variables and reset updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import { Header } from "./components/Header/Header";
 import { Hero } from "./components/Hero/Hero";
 import { Weather } from "./components/Weather/Weather";
@@ -7,9 +9,11 @@ import { Footer } from "./components/Footer/Footer";
 import { AuthModal } from "./components/AuthModal/AuthModal";
 
 export const App = function () {
+	const [modalOpen, setModalOpen] = useState(false);
+
 	return (
 		<>
-			<AuthModal open />
+			<AuthModal open={modalOpen} />
 
 			<Header />
 			<Hero />
