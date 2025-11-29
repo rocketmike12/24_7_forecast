c63d6b7dd12d6599fdece5db05dedeb78fc735ff
rocketmike12 <mg.korop@gmail.com>
2025-11-29 13:02:30 +0200

    added images; header markup done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,3 +1,6 @@
+import profileImg from "../../img/profile.png";
+import logo from "../../img/logo.svg";
+
 import { Container } from "../Container/Container";
 
 import styles from "./Header.module.scss";
@@ -5,7 +8,30 @@ import styles from "./Header.module.scss";
 export const Header = function () {
 	return (
 		<>
-			<Container>Header</Container>
+			<header className={styles["header"]}>
+				<Container>
+					<img src={logo} alt="logo" className={styles["header__logo"]} />
+
+					<nav className={styles["header__nav"]}>
+						<ul className={styles["header__nav-list"]}>
+							<li className={styles["header__nav-list-item"]}>
+								<p className={styles["header__nav-list-text"]}>Who we are</p>
+							</li>
+							<li className={styles["header__nav-list-item"]}>
+								<p className={styles["header__nav-list-text"]}>Contacts</p>
+							</li>
+							<li className={styles["header__nav-list-item"]}>
+								<p className={styles["header__nav-list-text"]}>Menu</p>
+							</li>
+						</ul>
+					</nav>
+
+					<div className={styles["header__profile"]}>
+						<button className={styles["header__profile-button"]}>Sign Up</button>
+						<img src={profileImg} alt="user" className={styles["header__profile-img"]} />
+					</div>
+				</Container>
+			</header>
 		</>
 	);
 };
